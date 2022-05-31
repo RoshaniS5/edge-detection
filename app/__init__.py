b8a50b9014fc9a16b7afd699931b4af1999c118d
Roshani Shrestha <roshanishrestha983@gmail.com>
2022-05-30 20:40:42 -0400

    made sobel into function, added beginning of canny

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,12 @@
 from flask import Flask, render_template
-import sobel
+from edgedetection import *
 
 app = Flask(__name__)
 
 @app.route("/")
 def image():
+    sobel("./static/flower.png")
     return render_template('index.html')
 if __name__ == "__main__":
+        app.debug = True
         app.run()
