e2149c6e052ad287b1ed69813f2817e8de3ffa1e
Roshani Shrestha <roshanishrestha983@gmail.com>
2022-06-01 18:34:05 -0400

    add gaussian blur

--- app/__init__.py
+++ app/__init__.py
@@ -5,7 +5,9 @@ app = Flask(__name__)
 
 @app.route("/")
 def image():
-    sobel("./static/flower.png")
+    sobel("./static/flower.png", "edges")
+    gaussianBlur("./static/flower.png")
+    sobel("./static/blur.png", "edges2")
     return render_template('index.html')
 if __name__ == "__main__":
         app.debug = True
--- app/edgedetection.py
+++ app/edgedetection.py
@@ -18,8 +18,39 @@ import matplotlib.pyplot as plt
 
 # plt.show()
 
+# gaussian blur
+def gaussianBlur(image):
+    im = Image.open(image).convert('1')
+    data = np.asarray(im)
+    data2 = data.astype(int)
+    gaussian = [[1, 2, 1], [2, 4, 2], [1, 2, 1]]
+    gauss = np.asarray(gaussian)
+    conv = []
+    for i in range(len(data2)):
+        row = []
+        for j in range(len(data2[i])):
+            convkernel = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+            for a in range(3):
+                for b in range(3):
+                    if i+a-1 < 0 or j+b-1 < 0 or i+a-1 > len(data2) - 1 or j+b-1 > len(data2[i]) - 1:
+                        convkernel[a][b] = 0
+                    else:
+                        convkernel[a][b] = data2[i+a-1][j+b-1] * gauss[a][b]
+            row.append(np.mean(convkernel))
+        conv.append(row)
+            # if i == 0 and j == 0:
+            #     print(datakernel)
+    data3 = np.asarray(conv)
+
+    data4 = np.array(data3,dtype='bool')
+
+    im2 = Image.fromarray(data4)
+
+    im2.save("static/blur.png") 
+    return im2
+
 # space for calculations - our own sobel function
-def sobel(image):
+def sobel(image, save):
     im = Image.open(image).convert('1')
     data = np.asarray(im)
     data2 = data.astype(int)
@@ -58,7 +89,7 @@ def sobel(image):
                     if i+a-1 < 0 or j+b-1 < 0 or i+a-1 > len(ydir) - 1 or j+b-1 > len(ydir[i]) - 1:
                         convkernel[a][b] = 0
                     else:
-                        convkernel[a][b] = ydir[i+a-1][j+b-1] * xkernel[a][b]
+                        convkernel[a][b] = ydir[i+a-1][j+b-1] * ykernel[a][b]
             val = np.mean(convkernel)
             conv[j][i] = np.sqrt(np.power(conv[j][i], 2) + np.power(val, 2))
 
@@ -69,7 +100,7 @@ def sobel(image):
 
     im2 = Image.fromarray(data4)
 
-    im2.save("static/edges.png")
+    im2.save("static/" + save + ".png")
 
     # fig, axs = plt.subplots(1)
 
