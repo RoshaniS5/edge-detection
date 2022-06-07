a5ffece09d9355c10daf41bbf386ca767739abb9
Roshani Shrestha <roshanishrestha983@gmail.com>
2022-06-06 21:15:22 -0400

    update file upload, explanations, add images to make site faster

--- app/__init__.py
+++ app/__init__.py
@@ -1,15 +1,26 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 from edgedetection import *
 
 app = Flask(__name__)
 
 @app.route("/")
 def image():
-    sobel("./static/flower.png", "edges")
-    gaussianBlur("./static/flower.png")
-    sobel("./static/blur.png", "edges2")
-    return render_template('index.html')
-    
+    # sobel("./static/flower.png", "edges")
+    # gaussianBlur("./static/flower.png")
+    # sobel("./static/blur.png", "edges2")
+    return render_template('index.html', res=False)
+
+@app.route("/submit", methods=['GET', 'POST'])
+def submit():
+    try:
+        uploadedfile = request.files['filename']
+        print(uploadedfile.filename)
+        uploadedfile.save("input.png")
+        sobel(uploadedfile, "result")
+        return render_template('index.html', res=True)
+    except:
+        return redirect("/")
+
 if __name__ == "__main__":
         app.debug = True
         app.run()
